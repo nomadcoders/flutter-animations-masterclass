59f45a46b91b2879070fd6e6f704ec1e67611d94
니꼬 <nico@nomadcoders.co>
2023-05-02 18:07:36 +0700

    1.1 Implicitly Animated Widgets

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -9,8 +9,48 @@ class ImplicitAnimationsScreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Implict Animations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedAlign(
+              duration: const Duration(seconds: 2),
+              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+              child: AnimatedOpacity(
+                opacity: _visible ? 1 : 0.2,
+                duration: const Duration(seconds: 2),
+                child: Container(
+                  width: size.width * 0.8,
+                  height: size.width * 0.8,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: _trigger,
+              child: const Text('Go!'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
