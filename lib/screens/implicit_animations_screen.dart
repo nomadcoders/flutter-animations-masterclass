17db0154142ec7e714f9b236e96b46da6014570a
니꼬 <nico@nomadcoders.co>
2023-05-03 10:52:40 +0700

    1.4 TweenAnimationBuilder

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -28,17 +28,20 @@ class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedContainer(
-              curve: Curves.elasticOut,
-              duration: const Duration(seconds: 2),
-              width: size.width * 0.8,
-              height: size.width * 0.8,
-              transform: Matrix4.rotationZ(_visible ? 1 : 0),
-              transformAlignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: _visible ? Colors.red : Colors.amber,
-                borderRadius: BorderRadius.circular(_visible ? 100 : 0),
+            TweenAnimationBuilder(
+              tween: ColorTween(
+                begin: Colors.yellow,
+                end: Colors.red,
               ),
+              curve: Curves.bounceInOut,
+              duration: const Duration(seconds: 5),
+              builder: (context, value, child) {
+                return Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/4/4f/Dash%2C_the_mascot_of_the_Dart_programming_language.png",
+                  color: value,
+                  colorBlendMode: BlendMode.colorBurn,
+                );
+              },
             ),
             const SizedBox(
               height: 50,
