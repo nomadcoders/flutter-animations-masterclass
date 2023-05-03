e735b828e26de831ff39a37e19f9a24bcaa8d964
니꼬 <nico@nomadcoders.co>
2023-05-03 12:21:31 +0700

    2.2 Animation Values

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ExplicitAnimationsScreen extends StatefulWidget {
@@ -12,11 +14,27 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
     with SingleTickerProviderStateMixin {
   late final AnimationController _animationController = AnimationController(
     vsync: this,
+    duration: const Duration(seconds: 10),
   );
+
+  void _play() {
+    _animationController.forward();
+  }
+
+  void _pause() {
+    _animationController.stop();
+  }
+
+  void _rewind() {
+    _animationController.reverse();
+  }
 
   @override
   void initState() {
     super.initState();
+    Timer.periodic(const Duration(milliseconds: 500), (timer) {
+      print(_animationController.value);
+    });
   }
 
   @override
@@ -24,6 +42,34 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
     return Scaffold(
       appBar: AppBar(
         title: const Text('Explicit Animations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "${_animationController.value}",
+              style: const TextStyle(fontSize: 58),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _play,
+                  child: const Text("Play"),
+                ),
+                ElevatedButton(
+                  onPressed: _pause,
+                  child: const Text("Pause"),
+                ),
+                ElevatedButton(
+                  onPressed: _rewind,
+                  child: const Text("Rewind"),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
