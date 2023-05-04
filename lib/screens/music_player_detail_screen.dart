cdec10352a9ad71c2147fee2e94ba3f37b958ed9
니꼬 <nico@nomadcoders.co>
2023-05-04 17:25:18 +0700

    5.7 Code Challenge

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -76,7 +76,53 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
                 progressValue: _progressController.value,
               ),
             ),
-          )
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Row(
+              children: const [
+                Text(
+                  "00:00",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "01:00",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Interstellar",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const Text(
+            "A Film By Christopher Nolan - Original Motion Picture Soundtrack",
+            maxLines: 1,
+            overflow: TextOverflow.visible,
+            style: TextStyle(fontSize: 18),
+          ),
         ],
       ),
     );
