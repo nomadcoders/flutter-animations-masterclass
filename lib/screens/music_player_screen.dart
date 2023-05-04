8f472fe86c400563d559967b9d60fed718aa09fb
니꼬 <nico@nomadcoders.co>
2023-05-04 16:25:03 +0700

    5.2 Album Scale

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -16,10 +16,27 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
 
   int _currentPage = 0;
 
+  final ValueNotifier<double> _scroll = ValueNotifier(0.0);
+
   void _onPageChanged(int newPage) {
     setState(() {
       _currentPage = newPage;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      if (_pageController.page == null) return;
+      _scroll.value = _pageController.page!;
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -60,25 +77,35 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
               return Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    height: 350,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.4),
-                          blurRadius: 10,
-                          spreadRadius: 2,
-                          offset: const Offset(0, 8),
-                        )
-                      ],
-                      image: DecorationImage(
-                        image: AssetImage(
-                          "assets/covers/${index + 1}.jpg",
+                  ValueListenableBuilder(
+                    valueListenable: _scroll,
+                    builder: (context, scroll, child) {
+                      final difference = (scroll - index).abs();
+                      final scale = 1 - (difference * 0.1);
+                      return Transform.scale(
+                        scale: scale,
+                        child: Container(
+                          height: 350,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.4),
+                                blurRadius: 10,
+                                spreadRadius: 2,
+                                offset: const Offset(0, 8),
+                              )
+                            ],
+                            image: DecorationImage(
+                              image: AssetImage(
+                                "assets/covers/${index + 1}.jpg",
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+                      );
+                    },
                   ),
                   const SizedBox(
                     height: 30,
