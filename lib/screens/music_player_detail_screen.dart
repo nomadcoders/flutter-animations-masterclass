3aa634ae994ba3af0f68b493eac7ed43c6fc8586
니꼬 <nico@nomadcoders.co>
2023-05-04 16:52:06 +0700

    5.4 PageRouteBuilder

---
+++ lib/screens/music_player_detail_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerDetailScreen extends StatefulWidget {
+  final int index;
+
+  const MusicPlayerDetailScreen({
+    super.key,
+    required this.index,
+  });
+
+  @override
+  State<MusicPlayerDetailScreen> createState() =>
+      _MusicPlayerDetailScreenState();
+}
+
+class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Interstellar'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Hero(
+              tag: "${widget.index}",
+              child: Container(
+                height: 350,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.4),
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                      offset: const Offset(0, 8),
+                    )
+                  ],
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "assets/covers/${widget.index}.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_animations_masterclass/screens/music_player_detail_screen.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
   const MusicPlayerScreen({super.key});
@@ -37,6 +38,22 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   void dispose() {
     _pageController.dispose();
     super.dispose();
+  }
+
+  void _onTap(int imageIndex) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return FadeTransition(
+            opacity: animation,
+            child: MusicPlayerDetailScreen(
+              index: imageIndex,
+            ),
+          );
+        },
+      ),
+    );
   }
 
   @override
@@ -82,25 +99,31 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                     builder: (context, scroll, child) {
                       final difference = (scroll - index).abs();
                       final scale = 1 - (difference * 0.1);
-                      return Transform.scale(
-                        scale: scale,
-                        child: Container(
-                          height: 350,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.4),
-                                blurRadius: 10,
-                                spreadRadius: 2,
-                                offset: const Offset(0, 8),
-                              )
-                            ],
-                            image: DecorationImage(
-                              image: AssetImage(
-                                "assets/covers/${index + 1}.jpg",
+                      return GestureDetector(
+                        onTap: () => _onTap(index + 1),
+                        child: Hero(
+                          tag: "${index + 1}",
+                          child: Transform.scale(
+                            scale: scale,
+                            child: Container(
+                              height: 350,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.4),
+                                    blurRadius: 10,
+                                    spreadRadius: 2,
+                                    offset: const Offset(0, 8),
+                                  )
+                                ],
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    "assets/covers/${index + 1}.jpg",
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                              fit: BoxFit.cover,
                             ),
                           ),
                         ),
