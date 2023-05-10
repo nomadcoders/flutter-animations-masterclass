8eb50911bd343907c54a145d9622f7993c9a7b90
니꼬 <nico@nomadcoders.co>
2023-05-10 13:35:55 +0700

    6.4 Custom Animation

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
 
 class RiveScreen extends StatefulWidget {
   const RiveScreen({super.key});
@@ -9,48 +8,11 @@ class RiveScreen extends StatefulWidget {
 }
 
 class _RiveScreenState extends State<RiveScreen> {
-  late final StateMachineController _stateMachineController;
-
-  void _onInit(Artboard artboard) {
-    _stateMachineController = StateMachineController.fromArtboard(
-      artboard,
-      "state",
-      onStateChange: (stateMachineName, stateName) {
-        print(stateMachineName);
-        print(stateName);
-      },
-    )!;
-    artboard.addController(_stateMachineController);
-  }
-
-  void _togglePanel() {
-    final input = _stateMachineController.findInput<bool>("panelActive")!;
-    input.change(!input.value);
-  }
-
-  @override
-  void dispose() {
-    _stateMachineController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Rive'),
-      ),
-      body: Center(
-        child: Container(
-          color: const Color(0xFFFF2ECC),
-          width: double.infinity,
-          child: RiveAnimation.asset(
-            "assets/animations/stars-animation.riv",
-            artboard: "artboard",
-            onInit: _onInit,
-            stateMachines: const ["state"],
-          ),
-        ),
       ),
     );
   }
