f6fc46ced562ff09b7b062dfeabb63a6ed55c5a1
니꼬 <nico@nomadcoders.co>
2023-05-10 14:13:03 +0700

    6.7 Conclusions

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -15,24 +13,20 @@ class _RiveScreenState extends State<RiveScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
-          const RiveAnimation.asset(
-            "assets/animations/balls-animation.riv",
-            fit: BoxFit.fill,
+        children: const [
+          RiveAnimation.asset(
+            "assets/animations/custom-button-animation.riv",
+            stateMachines: ["state"],
           ),
-          Positioned.fill(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
-              child: const Center(
-                child: Text(
-                  "Welcome to AI App",
-                  style: TextStyle(
-                    fontSize: 28,
-                  ),
-                ),
+          Center(
+            child: Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
               ),
             ),
-          ),
+          )
         ],
       ),
     );
